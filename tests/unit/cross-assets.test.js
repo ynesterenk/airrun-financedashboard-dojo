e52a90597a798c2ff48db40ca8ab678d1abe23d6
ynesterenk <yevgen2005@gmail.com>
2025-04-12 09:45:07 +0300

    REFIDMOPMO-5: Added cross-assets.test.js for unit testing

---
+++ tests/unit/cross-assets.test.js
@@ -0,0 +1,12 @@
+define(['dojo/_base/declare', 'dojo/_base/lang', 'dojo/dom', 'dojo/on', 'dojo/domReady!'], function(declare, lang, dom, on) {
+  return declare(null, {
+    constructor: function() {
+      console.log('Cross Assets Test Initialized');
+    },
+
+    runTests: function() {
+      console.log('Running Cross Assets Tests');
+      // Add specific test cases here
+    }
+  });
+});
